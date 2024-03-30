f67ed7042f53cb76beee9d817211a23cdab1d74d
halee-t <haleetisler@gmail.com>
2024-03-30 12:51:14 -0400

    css for hero section

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,16 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
+import "./components/Hero/hero.css";
+import Hero from "./components/Hero/Hero"; //import to connect the Hero.js file
+import data from "./TravelData"; //import to connect the TravelData.js file
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Hero />
     </div>
   );
 }
 
+//export to index.js
 export default App;
---
+++ my-app/src/components/Hero/Hero.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Background from "../../Assets/HeroSection-bg-image.jpg";
+
+function Hero() {
+  return (
+    <section className="hero-section">
+      <img
+        className="hero-img"
+        src={Background}
+        alt="paper airplane in the sunset"
+      />
+      <h1 className="hero-header">Welcome to the Travel Blog</h1>
+      <p className="hero-text">
+        Embark on Adventures: Inspiring Tales from Around the Globe. Join us as
+        we explore the world's hidden gems, indulge in local cuisines, and
+        connect with diverse cultures. Let our travel stories ignite your
+        wanderlust and guide you to unforgettable experiences.
+      </p>
+    </section>
+  );
+}
+
+export default Hero;
